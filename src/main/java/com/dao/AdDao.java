77ae6df33bec8b9c1b0c33761452da65dac2c470
wangyc <wangyc@example.com>
2016-08-09 16:00:03 +0800

    增加in操作

--- src/main/java/com/dao/AdDao.java
+++ src/main/java/com/dao/AdDao.java
@@ -1,7 +1,9 @@
 package com.dao;
 
 import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Select;
 
@@ -29,4 +31,22 @@ public interface AdDao {
 	@Select("select * from ad_base where ad_name like CONCAT('%',#{name},'%')")
 	@ResultMap("BaseResultMap")
 	AdBase getAdBaseByName(String name);
+	
+	//使用list参数来查询多个id
+	List<AdBase> findByIdsList(List<Long>ids);
+	
+	//使用Array参数来查询多个id
+	List<AdBase> findByIdsArray(Long[]ids);
+	
+	/**
+	 * 有多个参数的情况，且一个参数为id数组
+	 * 当查询的参数有多个时,例如 findByNameAndIds(String adName, Long[] ids)
+	 * 这种情况需要特别注意，在传参数时，一定要改用Map方式, 这样在collection属性可以指定名称也就是为ids
+	 * @param params
+	 * @return
+	 */
+	List<AdBase> findByNameAndIds(Map<String, Object> params);
+	
+	List<AdBase> findByNameAndId1(String adName,Long id);
+	List<AdBase> findByNameAndId2(@Param("adName") String adName,@Param("id") Long id);
 }
--- src/main/java/com/test/MyBatisTest.java
+++ src/main/java/com/test/MyBatisTest.java
@@ -1,7 +1,10 @@
 package com.test;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.junit.After;
@@ -14,58 +17,87 @@ import com.utils.MyBatisUtil;
 
 public class MyBatisTest {
 	private SqlSession sqlSession;
+	private AdDao adDao;
 
 	@Before
 	public void before() {
 		sqlSession = MyBatisUtil.getSessionFactory().openSession();
+		adDao = sqlSession.getMapper(AdDao.class);
 	}
 
 	@Test
 	public void findAll() {
-		System.out.println(sqlSession);
-		AdDao adDao = sqlSession.getMapper(AdDao.class);
 		List<AdBase> adBases = adDao.findAll();
 		System.out.println(adBases);
 	}
 
 	@Test
 	public void getAdBaseById() {
-		System.out.println(sqlSession);
-		AdDao adDao = sqlSession.getMapper(AdDao.class);
 		AdBase adBase = adDao.getAdBaseById(100);
 		System.out.println(adBase);
 	}
-	
+
 	@Test
 	public void getAdBaseByName() {
-		System.out.println(sqlSession);
-		AdDao adDao = sqlSession.getMapper(AdDao.class);
 		AdBase adBase = adDao.getAdBaseByName("test");
 		System.out.println(adBase);
 	}
 
 	@Test
-	public void save(){
+	public void save() {
 		AdBase adBase = new AdBase();
 		adBase.setAdName("广告1");
 		adBase.setPdateFrom(new Date());
 		adBase.setPdateEnd(new Date());
 		adBase.setCreateTime(new Date());
-		AdDao adDao = sqlSession.getMapper(AdDao.class);
 		System.out.println(adDao.save(adBase));
 		sqlSession.commit();
 	}
+
 	@Test
-	public void update(){
+	public void update() {
 		AdBase adBase = new AdBase();
 		adBase.setId(100);
 		adBase.setAdName("广告w23rw");
 		adBase.setPdateFrom(new Date());
 		adBase.setPdateEnd(new Date());
-		AdDao adDao = sqlSession.getMapper(AdDao.class);
 		adDao.update(adBase);
 		sqlSession.commit();
 	}
+
+	@Test
+	public void findByIdsList() {
+		List<AdBase> adBases = adDao.findByIdsList(Arrays.asList(new Long[] { 100L, 102L }));
+		System.out.println(adBases);
+	}
+
+	@Test
+	public void findByIdsArray() {
+		List<AdBase> adBases = adDao.findByIdsArray(new Long[] { 100L, 102L });
+		System.out.println(adBases);
+	}
+
+	@Test
+	public void findByNameAndIds() {
+		Map<String, Object> params = new HashMap<String, Object>(2);
+		params.put("adName", "w");
+		params.put("ids", Arrays.asList(new Long[] { 100L, 102L }));
+		//params.put("ids", new Long[] { 100L, 102L });
+		List<AdBase> adBases = adDao.findByNameAndIds(params);
+		System.out.println(adBases);
+	}
+	
+	@Test
+	public void findByNameAndId1() {
+		List<AdBase> adBases = adDao.findByNameAndId1("w",100L);
+		System.out.println(adBases);
+	}
+	@Test
+	public void findByNameAndId2() {
+		List<AdBase> adBases = adDao.findByNameAndId2("w",100L);
+		System.out.println(adBases);
+	}
+
 	@After
 	public void after() {
 		sqlSession.close();
